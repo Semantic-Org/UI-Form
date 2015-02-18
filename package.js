a7a3c10f93503068be04b3bb8ffae5e4c2703e68
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:32:41 -0500

    Updated component to version 1.9.1

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-form',
+  summary : 'Semantic UI - Form (official): Single component release of form',
+  version : '1.9.1',
+  git     : 'git://github.com/Semantic-Org/UI-Form.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
