6ba9cfaa18af6dd924c69cd994829cdf0b4efbb2
Pusher Robot <jack@semantic-ui.com>
2015-02-27 18:14:29 -0500

    Updated component to version 1.10.3

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-form',
   summary : 'Semantic UI - Form: Single component release',
-  version : '1.10.2',
+  version : '1.10.3',
   git     : 'git://github.com/Semantic-Org/UI-Form.git',
 });
 
