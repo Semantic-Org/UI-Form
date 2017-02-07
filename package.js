f37831dc75d63b8f08b84b375b1335249a6dc473
Jack Lukic <jack@semantic-ui.com>
2017-02-07 10:56:45 -0500

    Updated component to version 2.2.7

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-form',
   summary : 'Semantic UI - Form: Single component release',
-  version : '2.2.6',
+  version : '2.2.7',
   git     : 'git://github.com/Semantic-Org/UI-Form.git',
 });
 
